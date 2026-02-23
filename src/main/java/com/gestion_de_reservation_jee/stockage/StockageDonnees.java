88f9a1d2eafbc0a033734c1f3cf04667864a5d42
DESKTOP-7VN361H\Mdhamed <mohamedleminaidelha@gmail.com>
2026-02-23 18:05:55 +0000

    -Gestionnaire partis est terminé -Maintenant le gestionnaire peut consulter,valider et annuler les réservations, et peut aussi également offrir des services supplémentaires aux clients et signaler les dysfonctionnements des équipements des salles.

--- src/main/java/com/gestion_de_reservation_jee/stockage/StockageDonnees.java
+++ src/main/java/com/gestion_de_reservation_jee/stockage/StockageDonnees.java
@@ -3,13 +3,14 @@ package com.gestion_de_reservation_jee.stockage;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.gestion_de_reservation_jee.models.Reservation;
 import com.gestion_de_reservation_jee.models.Salle;
 import com.gestion_de_reservation_jee.models.User;
 
 public class StockageDonnees {
 	public static Map<String, User> users = new HashMap<>();
     public static Map<String, Salle> salles = new HashMap<>();
-
+    public static Map<String, Reservation> reservations = new HashMap<>();
     static {
         users.put("admin", new User("admin", "admin", "ADMIN", "", "admin@center.com"));
     }
